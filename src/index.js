03cfa05c251f9a62487ea87bcbad07a19fe7ed5a
EugeniaAlex <eugenia.aleksandrova@gmail.com>
2022-01-23 21:24:55 +0200

    instulation-of-libs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-import './sass/main.scss';
+// import './sass/main.scss';
+import cardElementTpl from '../templates/card-elements.hbs';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// const axios = require('axios');
+
+// axios.get('/user', {
+//     params: {
+//       ID: 12345
+//     }
+//   })
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // always executed
+//   }); 
+
+// --------------------------
+
+// async function getUser() {
+//   try {
+//     const response = await axios.get('/user?ID=12345');
+//     console.log(response);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
